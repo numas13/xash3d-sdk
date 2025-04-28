c2a4a95251f7026fc3f62020db9dabda3824c24d
Alibek Omarov <a1ba.omarov@gmail.com>
2025-04-29 00:47:57 +0300

    engine/ui: add gameinfo flags

--- engine/ui/raw.rs
+++ engine/ui/raw.rs
@@ -9,6 +9,7 @@ use core::{
     ptr,
 };
 
+use bitflags::bitflags;
 use csz::CStrArray;
 
 use crate::cvar::cvar_s;
@@ -38,7 +39,20 @@ pub struct GAMEINFO {
     pub gamemode: c_int,
 }
 
-#[derive(Copy, Clone)]
+bitflags! {
+    #[derive(Copy, Clone, PartialEq, Eq)]
+    #[repr(transparent)]
+    pub struct GameInfoFlags: u32 {
+        const NONE                  = 0;
+        const NOMODELS              = 1 << 0;
+        const NOSKILLS              = 1 << 1;
+        const RENDER_PICBUTTON_TEXT = 1 << 2;
+        const HD_BACKGROUND         = 1 << 3;
+        const ANIMATED_TITLE        = 1 << 4;
+    }
+}
+
+#[derive(Copy, Clone, PartialEq, Eq)]
 #[repr(C)]
 pub enum GameType {
     Normal = 0,
@@ -57,7 +71,7 @@ pub struct gameinfo2_s {
     pub title: CStrArray<64>,
     pub iconpath: CStrArray<64>,
     pub version: CStrArray<16>,
-    pub flags: u32,
+    pub flags: GameInfoFlags,
     pub game_url: CStrArray<256>,
     pub update_url: CStrArray<256>,
     pub type_: CStrArray<64>,
