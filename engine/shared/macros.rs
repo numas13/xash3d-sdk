7d02761cb15c731086b6527b6c7f4757ef8ad304
Denis Drakhnia <numas13@gmail.com>
2025-09-13 07:24:14 +0300

    engine/shared: add macro define_enum_for_primitive

--- engine/shared/macros.rs
+++ engine/shared/macros.rs
@@ -55,3 +55,70 @@ macro_rules! cstringify {
 }
 #[doc(inline)]
 pub use cstringify;
+
+/// Helper macro to define enums for primitive types.
+///
+/// # Examples
+///
+/// ```
+/// use xash3d_shared::macros::define_enum_for_primitive;
+///
+/// mod bindings {
+///     pub const TYPE_A: u8 = 0;
+///     pub const TYPE_B: u8 = 1;
+/// }
+///
+/// define_enum_for_primitive! {
+///     #[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
+///     pub enum Type: u8 {
+///         A(bindings::TYPE_A),
+///         B(bindings::TYPE_B),
+///         #[default]
+///         C(4),
+///     }
+/// }
+///
+/// assert_eq!(Type::A.into_raw(), bindings::TYPE_A);
+/// assert_eq!(Type::B.into_raw(), bindings::TYPE_B);
+/// assert_eq!(Type::C.into_raw(), 4);
+///
+/// assert_eq!(Type::from_raw(0), Some(Type::A));
+/// assert_eq!(Type::from_raw(1), Some(Type::B));
+/// assert_eq!(Type::from_raw(4), Some(Type::C));
+///
+/// assert_eq!(Type::from_raw(2), None);
+/// ```
+#[doc(hidden)]
+#[macro_export]
+macro_rules! define_enum_for_primitive {
+    (
+        $(#[$enum_attr:meta])*
+        $vis:vis enum $name:ident: $ty:ty {
+            $($(#[$variant_attr:meta])* $variant:ident($($value:tt)+)),+ $(,)?
+        }
+    ) => {
+        $(#[$enum_attr])*
+        $vis enum $name {
+            $($(#[$variant_attr])* $variant,)+
+        }
+
+        impl $name {
+            /// Creates an enum if the given raw value is valid.
+            pub fn from_raw(value: $ty) -> Option<Self> {
+                match value {
+                    $($($value)+ => Some(Self::$variant),)+
+                    _ => None,
+                }
+            }
+
+            /// Converts this enum to a raw value.
+            pub fn into_raw(self) -> $ty {
+                match self {
+                    $(Self::$variant => $($value)+ as $ty,)+
+                }
+            }
+        }
+    };
+}
+#[doc(inline)]
+pub use define_enum_for_primitive;
