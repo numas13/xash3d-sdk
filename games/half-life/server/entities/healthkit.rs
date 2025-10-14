955ed6d721af2c7b78f7a9ba89d2b1ced8bd45af
Denis Drakhnia <numas13@gmail.com>
2025-10-14 12:18:36 +0300

    half-life/server: set continuous use caps for func_healthcharger stub

--- games/half-life/server/entities/healthkit.rs
+++ games/half-life/server/entities/healthkit.rs
@@ -1,7 +1,37 @@
 use xash3d_server::{
-    entity::{Private, StubEntity},
+    entity::{
+        delegate_entity, impl_entity_cast, BaseEntity, CreateEntity, Entity, ObjectCaps, Private,
+        StubEntity,
+    },
     export::export_entity,
+    save::{Restore, Save},
 };
 
+#[derive(Save, Restore)]
+pub struct WallHealthCharger {
+    base: StubEntity,
+}
+
+impl_entity_cast!(WallHealthCharger);
+
+impl CreateEntity for WallHealthCharger {
+    fn create(base: BaseEntity) -> Self {
+        Self {
+            base: StubEntity::new(base, false),
+        }
+    }
+}
+
+impl Entity for WallHealthCharger {
+    delegate_entity!(base not { object_caps });
+
+    fn object_caps(&self) -> ObjectCaps {
+        self.base
+            .object_caps()
+            .difference(ObjectCaps::ACROSS_TRANSITION)
+            .union(ObjectCaps::CONTINUOUS_USE)
+    }
+}
+
 export_entity!(item_healthkit, Private<StubEntity>);
-export_entity!(func_healthcharger, Private<StubEntity>);
+export_entity!(func_healthcharger, Private<WallHealthCharger>);
